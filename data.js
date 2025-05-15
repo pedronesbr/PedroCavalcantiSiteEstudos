34de28ba90e20ede876bd2a63baef486f497e8e4
pedronesbr <81884678+pedronesbr@users.noreply.github.com>
2025-05-15 10:35:35 -0300

    Perfeição de Banco de Dados

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 /* data.js */
 window.listaQuestoes = [
-  {Disciplina:'Biologia', Assunto:'01', label:'SAS-2021-1-Q-94', QPDFName:'SAS_2021_1_Nat.pdf', page:[7], GPDFName:'SAS_2021_1_Nat_Gabarito.pdf', gabaritoPage:[19,20]},
+  {Disciplina:'Biologia', Assunto:'01', label:'SAS-2021-1-Q-109', QPDFName:'SAS_2021_1_Nat.pdf', page:[7], GPDFName:'SAS_2021_1_Nat_Gabarito.pdf', gabaritoPage:[19,20]},
 
 ];
